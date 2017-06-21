657d076f2d9c465ddd5400b41031e5ba0df834e6
j8298c <juliocmojica@gmail.com>
2017-06-20 23:28:57 -0400

    added mongoose connection and bodyparser also custom middleware

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
+const mongoose = require('mongoose');
+const Workout = require('./api/models/workoutModel');
+const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res)=>{
+   res.status(404).send({url: req.originalUrl + 'not found'});
+});
 
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db.url);
+
+const routes = require('./api/routes/workoutRoutes');
+routes(app);
 app.listen(port, ()=>{
     console.log(`app is now listening on port ${port}`);
 });
