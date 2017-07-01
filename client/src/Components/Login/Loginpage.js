2d149ceb534736707ce30fcd6580025974d7ae82
j8298c <juliocmojica@gmail.com>
2017-06-30 22:41:30 -0400

    login css added

--- client/src/Components/Login/Loginpage.js
+++ client/src/Components/Login/Loginpage.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import inputs from '../inputs';
 import buttons from '../buttons';
 class Login extends Component{
+    //needs to handle user state
     render(){
         return(
             <Form>
                 <inputs className="login-input" placeholder="Enter your username" />
                 <inputs className="login-input" placeholder="Enter your username" />
-                <buttons className="start-button app-button">Login</buttons>
+                <buttons className="mdl-button mdl-js-button mdl-button--raised startbutton app-button">Login</buttons>
             </Form>
         )
     }
