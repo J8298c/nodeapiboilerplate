fe3121ccfc65889ca4e2b55d8ab71e233ed552d9
j8298c <juliocmojica@gmail.com>
2017-07-02 15:11:49 -0400

    adding profile page

---
+++ client/src/Components/User/Profile.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Profile extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Welcome to the profile page</h1>
+            </div>
+        )
+    }
+}
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import {Route, BrowserRouter as Router} from 'react-router-dom';
 import SplashPage from './Components/SplashPage/SplashPage';
-import LoginPage from './Components/Login/Loginpage';
-import Signup from './Components/Login/Signup';
+import LoginPage from './Components/User/Loginpage';
+import Signup from './Components/User/Signup';
+import Profile from './Components/User/Profile';
+
+
+
 const routes = (
     <Router>
         <div>
             <Route exact path="/" component={SplashPage}/>
             <Route path="/login" component={LoginPage}/>
             <Route path="/signup" component={Signup} />
+            <Route path="/profile" component={Profile} />
         </div>
     </Router>
 )
