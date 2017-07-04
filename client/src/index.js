0156e9e3537537dc9f6fd42e02eaf77e56c495fa
j8298c <juliocmojica@gmail.com>
2017-07-04 12:37:15 -0400

    removed client to leave as back end api boiler plate

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import routes from './routes';
-
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(routes, document.getElementById('root'));
-registerServiceWorker();
